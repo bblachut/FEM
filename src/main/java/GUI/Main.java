490890a5b8d2237cf64f0c90cca315536bde9f6b
bblachut1 <bogojedi1@gmail.com>
2022-01-15 22:25:26 +0100

    it works!

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,4 +1,57 @@
 package GUI;
 
-public class Main {
+import Solver.Solver;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+import java.util.Arrays;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Solver solver = new Solver(3);
+        double[] result = solver.solve();
+        System.out.println(Arrays.toString(result));
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(600);
+        primaryStage.setScene(makeScene(primaryStage));
+        primaryStage.show();
+    }
+
+    private Scene makeScene(Stage primaryStage) {
+        TextField nElements = new javafx.scene.control.TextField("15");
+        Button button = new Button("draw graph");
+        button.setOnAction(event -> primaryStage.setScene(makeGraphScene(Integer.parseInt(nElements.getText()))));
+        return new Scene(new VBox(nElements, button));
+    }
+
+    private Scene makeGraphScene(int n) {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        lineChart.getData().add(series);
+        lineChart.setCreateSymbols(false);
+        lineChart.setLegendVisible(false);
+        Solver solver = new Solver(n);
+        double[] result = solver.solve();
+        for (double i = 0; i <= 2; i += 0.01) {
+            double value = 0;
+            for (int j = 0; j < n; j++) {
+                value += result[j] * solver.e(j, i);
+            }
+            series.getData().add(new XYChart.Data<>(i, value));
+        }
+        return new Scene(lineChart);
+    }
 }
---
+++ src/main/java/Solver/Solver.java
@@ -0,0 +1,90 @@
+package Solver;
+
+import org.apache.commons.math3.linear.*;
+
+import java.util.Arrays;
+
+public class Solver {
+    private final int n;
+
+    public Solver(int n) {
+        this.n = n;
+    }
+
+    public double[] solve() {
+        RealMatrix bMatrix = new Array2DRowRealMatrix(n, n);
+        RealVector lVector = new ArrayRealVector(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bMatrix.setEntry(i, j, B(i, j));
+            }
+            lVector.setEntry(i, L(i));
+        }
+        System.out.println(lVector);
+        System.out.println(bMatrix);
+        RealVector W = new LUDecomposition(bMatrix).getSolver().solve(lVector);
+
+        return Arrays.copyOf(W.toArray(), n);
+    }
+
+    private double integrate(int i, int j) {
+        double a_i = (2.0 / n) * (i - 1);
+        double a_j = (2.0 / n) * (j - 1);
+        double b_i = (2.0 / n) * (i + 1);
+        double b_j = (2.0 / n) * (j + 1);
+        if (a_j > b_i || a_i > b_j) {//it means that functions don't have common part
+            return 0.0;
+        }
+        double a = Math.max(0, Math.max(a_i, a_j));
+        double b = Math.min(2, Math.min(b_i, b_j));
+        double[] w = {5.0 / 9, 8.0 / 9, 5.0 / 9};
+        double[] x = {-((b - a) / 2) * Math.sqrt(3.0 / 5) + (b + a) / 2, (b + a) / 2, ((b - a) / 2) * Math.sqrt(3.0 / 5) + (b + a) / 2};
+        double res = 0;
+        for (int k = 0; k < w.length; k++) {
+            res += w[k] * eDx(i, x[k]) * eDx(j, x[k]);
+        }
+        return ((b - a) / 2) * res;
+    }
+
+    public double e(int i, double x) {
+        double h = 2.0 / n;
+        double xi = i * h;
+        double left = xi - h;
+        double right = xi + h;
+        if (x < left || x > right) {
+            return 0.0;
+        }
+        if (x < xi) {
+            return (x - left) / h;
+        }
+        return (right - x) / h;
+    }
+
+    private double eDx(int i, double x) {
+        double h = 2.0 / n;
+        double xi = i * h;
+        double left = Math.max(xi - h, 0.0);
+        double right = Math.min(xi + h, 2.0);
+        if (x <= left || x >= right) {
+            return 0.0;
+        }
+        if (x < xi) {
+            return (double) Math.round((1 / h) * 1000000000) / 1000000000;
+        }
+        return (double) Math.round((-1 / h) * 1000000000) / 1000000000;
+
+    }
+
+    private double B(int i, int j) {
+        return -e(i, 0) * e(j, 0) + integrate(i, j);
+    }
+
+    public double L(int m) {
+        return -20 * e(m, 0);
+    }
+
+//    private int k(double x){
+//        return x <= 1 ? 1 : 2;
+//    }
+
+}
